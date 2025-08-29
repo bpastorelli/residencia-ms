14de98dddc29b7450f4bbfd05f002b3eab503ebf
bpastorelli <brunopastorelli@hotmail.com>
2025-08-29 16:10:24 -0300

    Ajuste

--- src/main/java/br/com/residencia/security/config/WebSecurity.java
+++ src/main/java/br/com/residencia/security/config/WebSecurity.java
@@ -33,6 +33,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         http.authorizeRequests(requests -> requests
                 //.mvcMatchers(HttpMethod.GET, "https://j77akndu44.execute-api.sa-east-1.amazonaws.com/prod/**").permitAll()
                 .mvcMatchers(HttpMethod.GET, "**/sgc/residencia/**").permitAll()
+                .mvcMatchers(HttpMethod.POST, "**/sgc/residencia/**").permitAll()
+                .mvcMatchers(HttpMethod.PUT, "**/sgc/residencia/**").permitAll()
                 .antMatchers(HttpMethod.GET, AUTH_WHITELIST).permitAll()
                 .anyRequest()
                 .authenticated())
